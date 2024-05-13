7be6101033c6c27112a16f7717ee3c5147b0df4c
River Huang <rhuang@aglet.app>
2024-05-14 09:56:37 +1000

    simple button

--- lib/fancy_button.dart
+++ lib/fancy_button.dart
@@ -1,7 +1,40 @@
 library fancy_button;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class FancyButton extends StatelessWidget {
+  final GestureTapCallback? onPressed;
+  final String label;
+
+  FancyButton({this.onPressed, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      fillColor: Colors.deepOrange,
+      splashColor: Colors.orange,
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: 5.0,
+            ),
+            Text(
+              label,
+              maxLines: 1,
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+      onPressed: onPressed,
+      shape: const StadiumBorder(),
+    );
+  }
 }
