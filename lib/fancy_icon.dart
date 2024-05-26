60ecddddf357c667d75b1a27ec48306c0480e7dd
River Huang <rhuang@aglet.app>
2024-05-26 23:19:24 +1000

    update shimmer

--- lib/fancy_icon.dart
+++ lib/fancy_icon.dart
@@ -36,11 +36,6 @@ class _FancyIconState extends State<FancyIcon> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    final gradientStops = {
-      0.0: const Color(0xFFDF8600),
-      0.5: const Color(0xFFFCD981),
-      1.0: const Color(0xFFDF8600),
-    };
     const gradient = LinearGradient(
       begin: Alignment.topCenter,
       end: Alignment.bottomCenter,
@@ -50,33 +45,97 @@ class _FancyIconState extends State<FancyIcon> with TickerProviderStateMixin {
       ],
     );
     return Stack(
+      clipBehavior: Clip.antiAlias,
       alignment: Alignment.center,
       fit: StackFit.expand,
       children: [
-        Container(
-          padding: const EdgeInsets.all(4),
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-            gradient: gradient,
-          ),
-          child: Container(
-            clipBehavior: Clip.antiAlias,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.black,
+        AnimatedBuilder(
+            animation: _animation,
+            builder: (context, _) {
+              return Shimmer(
+                slidePercent: (_animation.value < 0.1 || _animation.value > 0.9)
+                    ? (_animation.value * 10) % 1.0
+                    : 0,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: gradient,
+                  ),
+                ),
+              );
+            }),
+        if (true)
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.black87,
+              ),
             ),
           ),
-        ),
-        ShaderMask(
-          shaderCallback: (rect) => gradient.createShader(rect),
-          blendMode: BlendMode.srcATop,
-          child: const Icon(
-            Icons.diamond,
-            color: Colors.white,
-            size: 20,
+        if (true)
+          ShaderMask(
+            shaderCallback: (rect) => gradient.createShader(rect),
+            blendMode: BlendMode.srcATop,
+            child: const Icon(
+              Icons.diamond,
+              color: Colors.white,
+              size: 20,
+            ),
           ),
-        ),
       ],
+    );
+  }
+}
+
+class Shimmer extends StatefulWidget {
+  final Widget child;
+  final double slidePercent;
+
+  const Shimmer({required this.child, this.slidePercent = 0.0, super.key});
+
+  @override
+  State<Shimmer> createState() => _ShimmerState();
+}
+
+class _ShimmerState extends State<Shimmer> {
+  bool get isSized =>
+      (context.findRenderObject() as RenderBox?)?.hasSize ?? false;
+
+  Size get size => (context.findRenderObject() as RenderBox).size;
+
+  static const _shimmerGradient = LinearGradient(
+    colors: [
+      Color(0x00EBEBF4),
+      Color(0xFFF4F4F4),
+      Color(0x00EBEBF4),
+    ],
+    stops: [
+      0.4,
+      0.5,
+      0.6,
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    tileMode: TileMode.clamp,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      blendMode: BlendMode.srcATop,
+      shaderCallback: (bounds) {
+        return _shimmerGradient.createShader(
+          Rect.fromLTWH(
+            (widget.slidePercent * 1.2 - 0.6) * size.width,
+            (widget.slidePercent * 1.2 - 0.6) * size.height,
+            size.width,
+            size.height,
+          ),
+        );
+      },
+      child: widget.child,
     );
   }
 }
